ba6689712334a7c1a1b84fb9316bdefb4dfcb2a5
dorukmaltepe <doruk152v@icloud.com>
2019-12-09 18:25:47 +0300

    Event class added

---
+++ src/Event.java
@@ -0,0 +1,9 @@
+public class Event {
+    private final String eventTaunt = "Taunt";
+    private final String eventCharge = "Charge";
+    private final String eventWF = "WF";
+    private final String eventBC = "BC";
+    private final String eventDR = "DR";
+    private final String eventStealth = "Stealth";
+
+}
